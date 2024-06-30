919c51730b93b276d0f9dcc19a28c9dd34e5991c
tranvusf19 <s3915185@rmit.edu.vn>
2024-06-30 19:40:25 +0700

    revised tts page

--- src/fragment/tts/LeftTabFragment.js
+++ src/fragment/tts/LeftTabFragment.js
@@ -1,58 +1,76 @@
-import { faFileAlt } from '@fortawesome/free-solid-svg-icons'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
-import { faFileAudio } from '@fortawesome/free-solid-svg-icons'
-import { faAudioDescription } from '@fortawesome/free-solid-svg-icons'
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
-import { faBookBookmark } from '@fortawesome/free-solid-svg-icons'
-import { faGear } from '@fortawesome/free-solid-svg-icons'
-import { faQuestion } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './LeftTabFragment.css'; // Import your CSS file for styles
 import personPortrait from '../../resources/images/person-portrait.jpg'
-
+import homeGray from '../../resources/images/home-gray.png'
+import ConvertTextBlue from '../../resources/images/convert-text-blue.png'
+import MicroGray from '../../resources/images/micro-gray.png'
+import CartGray from '../../resources/images/cart-gray.png'
+import bookGray from '../../resources/images/book-gray.png'
+import gearGray from '../../resources/images/gear-gray.png'
+import fileGray from '../../resources/images/file-gray.png'
+import questionMarkGray from '../../resources/images/questionmark-gray.png'
+import logo from '../../resources/images/logo.png'
 
 function LeftTabFragment() {
     return (
-        <div style={{ padding: '0px 0px 30px 0px', margin: '0', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '100%', height: '100%' }}>
-            <div className="custom-list-container">
-                <h1 className="custom-list-header" style={{ margin: '20px 20px', boxSizing: 'border-box', display: 'inline-block' }}>Name</h1>
-                <ul className="custom-ul" style={{ margin: '0px 10px', padding: 0, display: 'flex', flexDirection: 'column', alignItems: 'start', gap: '30px' }}>
-                    <li className="custom-list-item" style={{ margin: '0px 10px', padding: 0, display: 'flex', alignItems: 'center', gap: '30px' }}>
-                        <FontAwesomeIcon icon={faHome} color='gray' width={30}></FontAwesomeIcon>
-                        <a>Home</a>
-                    </li>
-                    <li className="custom-list-item" style={{ margin: '0px 10px', padding: 0, display: 'flex', alignItems: 'center', gap: '30px' }}>
-                        <FontAwesomeIcon icon={faFileAudio} color='gray' width={30}></FontAwesomeIcon>
-                        <a>Convert Text</a>
-                    </li>
-                    <li className="custom-list-item" style={{ margin: '0px 10px', padding: 0, display: 'flex', alignItems: 'center', gap: '30px' }}>
-                        <FontAwesomeIcon icon={faAudioDescription} color='gray' width={30}></FontAwesomeIcon>
-                        <a>Voice Cloning</a>
-                    </li>
-                    <li className="custom-list-item" style={{ margin: '0px 10px', padding: 0, display: 'flex', alignItems: 'center', gap: '30px' }}>
-                        <FontAwesomeIcon icon={faCartPlus} color='gray' width={30}></FontAwesomeIcon>
-                        <a>Buy Package</a>
-                    </li>
-                    <li className="custom-list-item" style={{ margin: '0px 10px', padding: 0, display: 'flex', alignItems: 'center', gap: '30px' }}>
-                        <FontAwesomeIcon icon={faBookBookmark} color='gray' width={30}></FontAwesomeIcon>
-                        <a>User Guide</a>
-                    </li>
-                    <li className="custom-list-item" style={{ margin: '0px 10px', padding: 0, display: 'flex', alignItems: 'center', gap: '30px' }}>
-                        <FontAwesomeIcon icon={faGear} color='gray' width={30}></FontAwesomeIcon>
-                        <a>Settings</a>
-                    </li>
-                </ul>
+        <div style={{ margin: '0', padding: '15px 30px 30px 30px', width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', gap: '20px' }}>
+
+            {/* TOP PART */}
+            <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'column', gap: '20px' }}>
+                <div style={{ margin: '0', padding: '0', display: 'flex', flexDirection: 'row', gap: '20px', alignItems: 'center', marginLeft: '-10px' }}>
+                    <img src={logo} style={{ width: '60px', height: '60px' }} alt="Description" />
+                    <p style={{ margin: '0', padding: '0', fontSize: '25px' }}>ChilliBilly</p>
+                </div>
+                <div style={{ margin: '0', padding: '0', width: '100%' }}>
+                    <p style={{ color: '#757575', fontSize: '11px', margin: '0', padding: '0', fontWeight: 'bold' }}>MAIN</p>
+                    <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'column', paddingTop: '10px', gap: '15px' }}>
+                        <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'row', gap: '15px', height: '30px', alignItems: 'center' }}>
+                            <img src={homeGray} style={{ width: '15px', height: '15px' }} alt="Description" />
+                            <p style={{ color: '#757575', fontSize: '14px', margin: '0', padding: '0' }}>Home</p>
+                        </div>
+                        <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'row', gap: '15px', height: '30px', alignItems: 'center' }}>
+                            <img src={ConvertTextBlue} style={{ width: '15px', height: '15px' }} alt="Description" />
+                            <p style={{ color: '#367AFF', fontSize: '14px', margin: '0', padding: '0' }}>Convert Text</p>
+                        </div>
+                        <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'row', gap: '15px', height: '30px', alignItems: 'center' }}>
+                            <img src={MicroGray} style={{ width: '15px', height: '15px' }} alt="Description" />
+                            <p style={{ color: '#757575', fontSize: '14px', margin: '0', padding: '0' }}>Voice Cloning</p>
+                        </div>
+                        <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'row', gap: '15px', height: '30px', alignItems: 'center' }}>
+                            <img src={CartGray} style={{ width: '15px', height: '15px' }} alt="Description" />
+                            <p style={{ color: '#757575', fontSize: '14px', margin: '0', padding: '0' }}>Buy Package</p>
+                        </div>
+                        <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'row', gap: '15px', height: '30px', alignItems: 'center' }}>
+                            <img src={bookGray} style={{ width: '15px', height: '15px' }} alt="Description" />
+                            <p style={{ color: '#757575', fontSize: '14px', margin: '0', padding: '0' }}>User Guide</p>
+                        </div>
+                    </div>
+                </div>
+                <div style={{ margin: '0', padding: '0', width: '100%' }}>
+                    <p style={{ color: '#757575', fontSize: '11px', margin: '0', padding: '0', fontWeight: 'bold' }}>SETTING</p>
+                    <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'column', paddingTop: '10px', gap: '15px' }}>
+                        <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'row', gap: '15px', height: '30px', alignItems: 'center' }}>
+                            <img src={gearGray} style={{ width: '15px', height: '15px' }} alt="Description" />
+                            <p style={{ color: '#757575', fontSize: '14px', margin: '0', padding: '0' }}>Settings</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div style={{ margin: '0', padding: '0px 20px', display: 'flex', flexDirection: 'column', gap: '29px', width: '100%' }}>
-                <div style={{ margin: '0 10px', padding: '0', display: 'flex', flexDirection: 'row', gap: '20px', alignItems: 'center' }}>
-                    <FontAwesomeIcon icon={faFileAlt} color='gray' width={30}></FontAwesomeIcon>
-                    <p style={{ margin: '0px 20px', padding: '0' }}>Docs and Resources</p>
+
+            {/* BOTTOM PART */}
+            <div style={{ margin: '0', padding: '0', width: '100%', display: 'flex', flexDirection: 'column', gap: '15px' }}>
+                <div style={{ margin: '0', padding: '0', display: 'flex', flexDirection: 'row', gap: '15px', alignItems: 'center' }}>
+                    <div style={{ width: '30px', height: '15px', display: 'flex', justifyContent: 'center' }}>
+                        <img src={fileGray} style={{ width: '15px', height: '15px' }} alt="Description" />
+                    </div>
+                    <p style={{ margin: '0px', padding: '0' }}>Docs and Resources</p>
                 </div>
-                <div style={{ margin: '0 10px', padding: '0', display: 'flex', flexDirection: 'row', gap: '20px', alignItems: 'center' }}>
-                    <FontAwesomeIcon icon={faQuestion} color='gray' width={30}></FontAwesomeIcon>
-                    <p style={{ margin: '0px 20px', padding: '0' }}>FAQs</p>
+                <div style={{ margin: '0', padding: '0', display: 'flex', flexDirection: 'row', gap: '15px', alignItems: 'center' }}>
+                    <div style={{ width: '30px', height: '15px', display: 'flex', justifyContent: 'center' }}>
+                        <img src={questionMarkGray} style={{ width: '15px', height: '15px' }} alt="Description" />
+                    </div>
+                    <p style={{ margin: '0px', padding: '0' }}>FAQs</p>
                 </div>
-                <div style={{ margin: '0', padding: '0', display: 'flex', flexDirection: 'row', width: '100%', gap: '30px' }}>
+                <div style={{ margin: '0', padding: '0', display: 'flex', flexDirection: 'row', width: '100%', gap: '15px', alignItems: 'center' }}>
                     <div className="image-content">
                         <img src={personPortrait} alt="Description" />
                     </div>
--- src/perspective/TextToSpeechPage.js
+++ src/perspective/TextToSpeechPage.js
@@ -2,23 +2,19 @@ import './TextToSpeechPage.css';
 import HeaderTabComponent from '../component/tts/HeaderTabComponent';
 import InputTabComponent from '../component/tts/InputTabComponent';
 import LeftTabComponent from '../component/tts/LeftTabComponent';
-import SoundDisplayTabComponent from '../component/tts/SoundDisplayTabComponent';
 
 function TextToSpeechPage() {
     return (
-        <div className="flex-container">
-            <div style={{ borderRight: '1px solid #dddddd' }}>
+        <div className="flex-container" style={{ display: 'flex', height: '100vh', width: '100vw' }}>
+            <div style={{ borderRight: '1px solid #eeeeee', flex: '0 0 auto', width: '250px' }}>
                 <LeftTabComponent />
             </div>
-            <div className="flex-column">
+            <div className="flex-column" style={{ height: '100%', width: '100%', position: 'relative' }}>
                 <div style={{ margin: '0', padding: '0', height: '11%', minHeight: '120px' }}>
                     <HeaderTabComponent />
                 </div>
-                <div style={{ margin: '0', padding: '0', height: '70%' }}>
+                <div style={{ margin: '0', padding: '0', height: '89%', width: '100%', overflowY: 'hidden' }}>
                     <InputTabComponent />
-                </div>
-                <div style={{ margin: '0', padding: '0', height: '19%' }}>
-                    <SoundDisplayTabComponent />
                 </div>
             </div>
         </div>

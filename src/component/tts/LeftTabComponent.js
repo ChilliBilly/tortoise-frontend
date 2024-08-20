afd0271c0b8f2d14a3638237742bccb125beba7e
MoryDog <s3881103@rmit.edu.vn>
2024-08-21 01:44:13 +0700

    add logout button

--- src/component/tts/LeftTabComponent.js
+++ src/component/tts/LeftTabComponent.js
@@ -1,11 +1,17 @@
-import LeftTabFragment from "../../fragment/tts/LeftTabFragment"
+import LeftTabFragment from "../../fragment/tts/LeftTabFragment";
 
 function LeftTabComponent() {
+    const handleLogout = () => {
+        // Clear user session and redirect to login
+        localStorage.removeItem('token');
+        window.location.href = '/login'; // Redirect to login page
+    };
+
     return (
         <div style={{ width: '100%', height: '100%' }}>
-            <LeftTabFragment />
+            <LeftTabFragment onLogout={handleLogout} />
         </div>
-    )
+    );
 }
 
-export default LeftTabComponent
+export default LeftTabComponent;
